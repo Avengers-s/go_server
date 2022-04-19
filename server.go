69bdc4511e81e272fcf0d4f7fdd499a3474b8738
dylolorz <yiding0216@163.com>
2022-04-19 20:21:56 +0800

    write user

--- server.go
+++ server.go
@@ -3,20 +3,52 @@ package main
 import(
     "fmt"
     "net"
+    "sync"
 )
 
 type Server struct{
     Ip string
     Port int
+    OnlineMap map[string]*User
+    Message chan string
+    MapLock sync.RWMutex
 }
 
 func NewServer(ip string, port int) *Server{
-    server := &Server{ip, port}
+    server := &Server{
+        Ip: ip,
+        Port: port,
+        OnlineMap: make(map[string]*User),
+        Message: make(chan string),
+    }
     return server
 }
 
 func (s *Server) Handler(conn net.Conn){
-    fmt.Println("连接成功!!!")
+    user := NewUser(conn)
+    s.MapLock.Lock()
+    s.OnlineMap[user.Name] = user
+    s.MapLock.Unlock()
+
+    s.BoardCast(user, "已上线")
+    select{}
+}
+
+func (s *Server) BoardCast(user *User, msg string){
+    sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+    s.Message <- sendMsg
+}
+
+func (s *Server) ListenMessage (){
+    for{
+        msg := <-s.Message
+        s.MapLock.Lock()
+        for _, cli := range s.OnlineMap{
+            cli.C <- msg
+        }
+        s.MapLock.Unlock()
+    }
 }
 
 func (s *Server) Start(){
@@ -26,7 +58,7 @@ func (s *Server) Start(){
         return 
     }
     defer listener.Close()
-
+    go s.ListenMessage()
     for{
         conn, err := listener.Accept()
         if err != nil {
